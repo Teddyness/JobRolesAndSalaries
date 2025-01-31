72b106a8cef5ae976b56ca67fe5c6ea1262d2461
Your Name <you@example.com>
2025-01-31 01:45:46 -0500

    salaryData 2 functions

--- modules/salaryData.js
+++ modules/salaryData.js
@@ -1,14 +1,35 @@
-const salaryData = [
-    { role: 'CTO', company: 'Big Data Inc.', salary: 320000 },
-    { role: 'Technical Lead', company: 'Big Data Inc.', salary: 230000 },
-    { role: 'Software Engineer II', company: 'Big Data Inc.', salary: 180000 },
-    { role: 'Software Engineer I', company: 'Big Data Inc.', salary: 140000 },
-    { role: 'CTO', company: 'Medium Data Inc.', salary: 215000 },
-    { role: 'Technical Lead', company: 'Medium Data Inc.', salary: 165000 },
-    { role: 'Software Engineer II', company: 'Medium Data Inc.', salary: 140000 },
-    { role: 'Software Engineer I', company: 'Medium Data Inc.', salary: 115000 },
-    { role: 'CTO', company: 'Small Data Inc.', salary: 175000 },
-    { role: 'Technical Lead', company: 'Small Data Inc.', salary: 135000 },
-    { role: 'Software Engineer II', company: 'Small Data Inc.', salary: 115000 },
-    { role: 'Software Engineer I', company: 'Small Data Inc.', salary: 95000 },
-  ];
+const salaryData = 
+[
+  {role:'CTO', company: 'Amazon', salary: 320000}, 
+  {role: 'Technical Lead', company: 'Amazon', salary: 230000}, 
+  {role: 'Software Engineer II', company: 'Amazon', salary: 180000},
+  {role: 'Software Engineer I', company: 'Amazon', salary: 140000},
+  {role: 'CTO', company: 'Microsoft', salary: 215000}, 
+  {role: 'Technical Lead', company: 'Microsoft', salary: 165000}, 
+  {role: 'Software Engineer II', company: 'Microsoft', }, 
+  {role: 'Software Engineer I', company: 'Microsoft', salary: 115000}, 
+  {role: 'CTO', company: 'Google', salary: 420000}, 
+  {role: 'Technical Lead', company: 'Google', salary: 330000},
+  {role: 'Software Engineer II', company: 'Google', salary: 280000},
+  {role: 'Software Engineer I', company: 'Google', Salary: 240000}
+]
+
+const getRoles = () =>{
+  let roles = salaryData.map(obj=>{
+    return obj.role;
+  })
+  roles = roles.filter((value, index, array) =>{
+    return array.indexOf(value)===index;
+  })
+  return roles;
+}
+
+const getCompanies = () =>{
+  let companies = salaryData.map(obj =>{
+    return obj.company;
+  })
+  companies = companies.filter((value, index, array)=>{
+    return array.indexOf(value)===index;
+  }) 
+  return companies;
+}
